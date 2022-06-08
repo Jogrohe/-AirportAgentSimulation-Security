f3d23877cf5e58f52e5598057d5b3e6d363de2b0
vNmven0m <72264500+vNmven0m@users.noreply.github.com>
2022-06-08 12:24:53 +0200

    Add files via upload

---
+++ AirportAgentSimulation-Security-main/src/main/java/plugin/SecurityPlugin.java
@@ -0,0 +1,22 @@
+package plugin;
+
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.AirportAgentSimulation;
+import dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.config.ConfigurationFormatException;
+import entities.officer;
+
+public class SecurityPlugin implements dhbw.sose2022.softwareengineering.airportagentsim.simulation.api.plugin.Plugin {
+
+
+    @Override
+    public void activate() {
+        try {
+            AirportAgentSimulation.registerEntity(this, "officer", officer.class);
+;
+        } catch (ConfigurationFormatException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
